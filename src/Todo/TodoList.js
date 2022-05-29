308ac5efd5330a3737c5038d6edc857ddca4f10d
Juli Mostova <julia.mostova89@gmail.com>
2022-05-29 23:08:28 +0300

    change todolist

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,31 +1,31 @@
 import React from "react";
-import PropTypes  from "prop-types";
+import PropTypes from 'prop-types';
 import TodoItem from "./TodoItem";
+
 
 
 
 const styles = {
     ul:{
-       listStyle:'none' ,
-       margin:0,
-       padding:0,
-       
+listStyle:'none',
+margin: 0,
+padding: 0
+
     }
 }
 
- function TodoList(props){
-return(
-    <ul style={styles.ul}>
-        {props.todos.map((todo, index)=>{
-            return <TodoItem todo={todo} key={todo.id} index={index} onChange={props.onToggle}></TodoItem>
-        }
-            )}
-    </ul>
-)}
-
-
-TodoList.propTypes= {
-    todos:PropTypes.arrayOf(PropTypes.object).isRequired,
-    onToggle:PropTypes.func.isRequired
+ function TodoList (props){
+    return (
+        <ul style ={styles.ul}>
+           {props.todos.map(todo =>{
+               return <TodoItem todo={todo} key={todo.id}></TodoItem>
+           })}
+        </ul>
+    )
 }
+
+TodoList.propTypes ={
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
 export default TodoList
