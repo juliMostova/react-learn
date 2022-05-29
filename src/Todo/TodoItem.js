a4dbbf20fecb9920adcbf8f3c94f017df036c881
Juli Mostova <julia.mostova89@gmail.com>
2022-05-29 23:08:20 +0300

    change todolist

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,56 +1,34 @@
-import React, {useContext} from "react";
-import PropTypes  from "prop-types";
-import Context from "../Context";
+import React from "react";
+import PropTypes from 'prop-types';
 
-
-
-
-const styles ={
-li: {
+const styles = {
+li:{
 display:'flex',
-justifyContent:'space-beetween',
-alignItems:'center',
-padding:'.5rem 1rem',
-border:'1px solid #ccc',
-borderRadius:'4px',
-marginBottom:'.5rem'
-},
-marginRighr:'1rem'
+justifyContent:'space-between',
+ alignItems:'center',
+ padding:'.5rem 1rem',
+ border:'1px solid #ccc',
+ borderRadius:'4px',
+ marginBottom:'.2rem'
 }
 
 
- function TodoItem({todo, index, onChange}){
-    const { removeTodo }= useContext(Context) 
-    const classes = []
-
-if(todo.completed) {
-    classes.push('done')
 }
-
-
+ function TodoItem ({todo}){
     return (
-        <li style={styles.li}>
-          <span className={classes.join(' ')}>
-              <input
-               type ='checkbox' checked={todo.completed}
-               style={styles.input}
-               onChange={()=> onChange(todo.id)}></input>
-              
-            <strong>{index+1}</strong>
-            &nbsp;
-            {todo.title}
-
-            </span>
-            <button className='rm' onClick={removeTodo.bind(null,todo.id)}> &times;</button>
-
-            </li>
-    )}
-
-
+        <li style ={styles.li}>
+         <span>
+             <input type ='checkbox'></input>
+         {todo.title}
+             </span>   
+            <button className='click'>Done</button>
+             </li>  
+       
+    )
+}
 TodoItem.propTypes ={
     todo:PropTypes.object.isRequired,
-    index:PropTypes.number,
-    onChange:PropTypes.func.isRequired
-}
 
-    export default TodoItem
+} 
+
+export default TodoItem
